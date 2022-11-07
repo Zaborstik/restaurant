cda2c53bcc4b1702bd2050c5438cf87838cbcaf4
Zaborstik <zaborstik@gmail.com>
2022-11-07 16:33:54 +0500

    Modded

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,12 +1,17 @@
 package src;
 
 import src.kitchen.Cook;
+import src.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
         Cook cook = new Cook("Jon");
         tablet.addObserver(cook);
+
+        Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
+
 
         tablet.createOrder();
         tablet.createOrder();
